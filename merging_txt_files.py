6ed3958e77e0e4b5a4a045e1a435f9c05838c3ba
AmeerAlkhier <36737273+AmeerAlkhier@users.noreply.github.com>
2021-07-06 12:47:26 +0200

    Update merging_txt_files.py

--- merging_txt_files.py
+++ merging_txt_files.py
@@ -1,38 +1,26 @@
 import os
 
-
 list_filenames =[]
-
 directory = r'/home/ameer/Downloads/ArabicBible'
+
 for filename in os.listdir(directory):
     if filename.endswith(".txt") :
         list_filenames.append(filename)
     else:
         continue
+        
 #print (len(list_filenames))
-
-
-
-
-
-
+# Create a file in write mode with the correct encoding to accumulate the other files in
 with open('bible.txt', 'w',encoding='utf-16') as outfile:
   
     # Iterate through list
     for names in list_filenames:
 
   
-        # Open each file in read mode
+        # Open each file in read mode with the correct encoding
         with open(names,encoding='utf-16') as infile:
   
             # read the data from the files and write them in bible.txt
-
-            #with open(infile, 'rb') as f:
             contents = infile.read()
             outfile.write(contents)
-
-            #readfile=infile.read()
-            #outfile.write(readfile)
-  
-        # Add '\n' to enter data of from next file 
-        outfile.write("++++")
+            
